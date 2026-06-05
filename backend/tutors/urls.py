c86e66e0e272cb7e980b5cd49375e3d24fbc7ed7
jsy002514 <jsy002514@naver.com>
2026-06-05 09:50:22 +0900

    feat(load_term): add new files

--- backend/tutors/urls.py
+++ backend/tutors/urls.py
@@ -9,7 +9,7 @@ router.register(r'terms', views.TermViewSet)
 router.register(r'daily-terms', views.DailyTermViewSet)
 router.register(r'quizzes', views.QuizViewSet)
 router.register(r'quiz-history', views.UserQuizHistoryViewSet, basename='quiz-history')
-
+router.register(r'review-quiz', views.ReviewQuizViewSet, basename='review-quiz')
 urlpatterns = [
     path('', include(router.urls)),
 ]
